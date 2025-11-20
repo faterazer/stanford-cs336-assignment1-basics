33030b45665e4566c5955a9b2baf060bf008a5c0
yubowen.ssr <yubowen.ssr@bytedance.com>
2025-11-20 20:28:15 +0800

    实现 Embedding 模块

--- cs336_basics/transformer.py
+++ cs336_basics/transformer.py
@@ -9,7 +9,16 @@ class Linear(nn.Module):
         super().__init__()
         self.weight = nn.Parameter(torch.empty((out_features, in_features), device=device, dtype=dtype))
         std = math.sqrt(2 / (in_features + out_features))
-        torch.nn.init.trunc_normal_(self.weight, 0, std, -3 * std, 3 * std)
+        nn.init.trunc_normal_(self.weight, mean=0, std=std, a=-3 * std, b=3 * std)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x @ self.weight.T
+
+class Embedding(nn.Module):
+    def __init__(self, num_embeddings: int, embedding_dim: int, device: torch.device = None, dtype: torch.dtype = None) -> None:
+        super().__init__()
+        self.weight = nn.Parameter(torch.empty((num_embeddings, embedding_dim), device=device, dtype=dtype))
+        nn.init.trunc_normal_(self.weight, mean=0, std=1, a=-3, b=3)
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        return self.weight[token_ids]
--- tests/adapters.py
+++ tests/adapters.py
@@ -52,8 +52,10 @@ def run_embedding(
     Returns:
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
-
-    raise NotImplementedError
+    from cs336_basics.transformer import Embedding
+    embedding = Embedding(vocab_size, d_model)
+    embedding.load_state_dict({"weight": weights})
+    return embedding(token_ids)
 
 
 def run_swiglu(
